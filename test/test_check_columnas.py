e3ba59e07751bac81d1f99a35bf043bd5cff65da
David Gelpi <gelpiorama@gmail.com>
2024-10-29 00:29:24 +0100

    test(test): Escribir casos test que precisan barricada previa

--- test/test_check_columnas.py
+++ test/test_check_columnas.py
@@ -1,18 +1,18 @@
 import pytest
-from src.checkFilas import checkFilas
+from src.checkColumnas import checkColumnas
 import casosTest.casosTestSudoku as casosTest
 
 
 @pytest.mark.parametrize("sudoku, sano",
                          [  (casosTest.correcto, True),
                             (casosTest.numero_repetido_fila_columna, False),
-                            (casosTest.numero_repetido_columna, True),
+                            (casosTest.numero_repetido_columna, False),
                             (casosTest.numero_no_presente, False),
-                            (casosTest.numero_fuera_del_rango, True), # barricada
+                            (casosTest.numero_fuera_del_rango, False), # except
                             # (casosTest.caracteres, True),
                             # (casosTest.numeros_reales, True),
                             # (casosTest.irregular_fila, True),
                             # (casosTest.irregular_columna, True)
                             ])
-def test_check_filas(sudoku, sano):
-    assert checkFilas(sudoku) == sano
+def test_check_columnas(sudoku, sano):
+    assert checkColumnas(sudoku) == sano
---
+++ test/test_check_sudoku.py
@@ -0,0 +1,17 @@
+import pytest
+from src.checkSudoku import checkSudoku
+import casosTest.casosTestSudoku as casosTest
+
+
+@pytest.mark.parametrize("sudoku, sano",
+                         [  (casosTest.correcto, True),
+                            (casosTest.numero_repetido_fila_columna, False),
+                            (casosTest.numero_repetido_columna, False),
+                            (casosTest.numero_no_presente, False),
+                            (casosTest.numero_fuera_del_rango, False),
+                            (casosTest.caracteres, False),
+                            (casosTest.numeros_reales, False),
+                            (casosTest.irregular_fila, False),
+                            (casosTest.irregular_columna, False)])
+def test_check_sudoku(sudoku, sano):
+    assert checkSudoku(sudoku) == sano
